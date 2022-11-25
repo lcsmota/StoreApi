dbc1fd1509769921ecce968f9a108f7421f8169c
lcsmota <sacul_m@protonmail.com>
2022-11-25 09:33:53 -0300

    Add Store model

---
+++ Models/Store.cs
@@ -0,0 +1,12 @@
+namespace StoreApi.Models;
+
+public class Store
+{
+    public int StoreId { get; set; }
+    public string StoreName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+}
